7bc1bf586d6a2b4f984cb91140d5c1afb13a5f82
jhanavi327 <132565126+jhanavi327@users.noreply.github.com>
2023-07-19 19:43:15 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package sayhello;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		String my_string ="JAVA IS FUN";
+		int N = 5;
+		for (int i = 0; i<N; i++)
+		{
+			System.out.println(my_string);
+		}
+	}
+
+}
